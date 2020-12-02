26bfbb72206a0d2ddf116f7f39e60590758795af
Saad Kureshi <saq619@gmail.com>
2020-12-02 01:24:43 +0000

    Updated

---
+++ asyncBreeds.js
@@ -0,0 +1,14 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) callback(data);
+  });
+};
+
+breedDetailsFromFile('Bombay', data => {
+  console.log('Return Value: ', data);
+});
