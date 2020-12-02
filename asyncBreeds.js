ac1d8f93dada53a943d75496b10bb624fba30fd7
Saad Kureshi <saq619@gmail.com>
2020-12-02 04:09:49 +0000

    Updated

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -5,10 +5,16 @@ const breedDetailsFromFile = function(breed, callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
-    if (!error) callback(data);
+    if (!error) {
+      callback(data);
+    } else {
+      callback(undefined);
+    }
   });
 };
 
-breedDetailsFromFile('Bombay', data => {
-  console.log('Return Value: ', data);
-});
+// breedDetailsFromFile('Bombay', data => {
+//   console.log('Return Value: ', data);
+// });
+
+module.exports = breedDetailsFromFile;
